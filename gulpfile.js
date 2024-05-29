4f43b3dd0497e1de48079cb6f6576a15a13e0229
sidmfkid <newskinlifestore@gmail.com>
2024-05-29 17:19:27 -0400

    fix for header

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 gulp.task("sass", function () {
-  return gulp.src("styles/**.scss").pipe(sass()).pipe(gulp.dest("assets"));
+  return gulp.src("styles/**/**.scss").pipe(sass()).pipe(gulp.dest("assets"));
 });
 
 gulp.task("watch", function () {
